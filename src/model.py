11fb3f7fcba2f04ccecb31593f91a7f0ca5ca63b
Derin <derinjv@gmail.com>
2026-02-13 15:33:44 +0530

    fix: correct EfficientNet model definition

--- src/model.py
+++ src/model.py
@@ -1,8 +1,13 @@
 import torch.nn as nn
 from torchvision import models
-def get_model(num_classes):
-    model=models.googlenet(weights="DEFAULT")
-    for param in model.parameters():
-        param.requires_grad=True
-    model.fc=nn.Linear(model.fc.in_features,num_classes)
-    return model    
+
+def get_model(num_classes, pretrained=True):
+    model = models.efficientnet_b0(
+        weights="DEFAULT" if pretrained else None
+    )
+
+    for param in model.features.parameters():
+        param.requires_grad = False
+    in_features = model.classifier[1].in_features
+    model.classifier[1] = nn.Linear(in_features, num_classes)
+    return model
